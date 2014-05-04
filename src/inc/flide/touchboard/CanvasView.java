a146b9bc150cee19691b69f6b18e16f0cd9aa6a9
flide <flideravi@gmail.com>
2014-05-04 23:11:34 +0530

    Issue with clearing of screen on reorientation fixed

--- src/inc/flide/touchboard/CanvasView.java
+++ src/inc/flide/touchboard/CanvasView.java
@@ -1,5 +1,6 @@
 package inc.flide.touchboard;
 
+import android.util.Log;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
@@ -7,15 +8,19 @@ import android.graphics.Canvas;
 import android.graphics.Bitmap;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Matrix;
 import android.view.MotionEvent;
 
 
-public class CanvasView extends View {
+public class CanvasView extends View 
+{
 
+	private static final String logTag = "CanvasView";
+	static int rotatePositive;
 	//drawing path
-	private Path drawPath;
+	private Path path;
 	//drawing and canvas paint
-	private Paint drawPaint, canvasPaint;
+	private Paint paint, canvasPaint;
 	//initial color
 	private int paintColor = 0xFF000000;
 	//canvas
@@ -23,71 +28,104 @@ public class CanvasView extends View {
 	//canvas bitmap
 	private Bitmap canvasBitmap;
 	
-	public CanvasView(Context context) {
+	public CanvasView(Context context) 
+	{
 		super(context);
-		// TODO Auto-generated constructor stub
+		Log.v(logTag, "CanvasView(Context) Started");
 		SetupDrawing();
+		Log.v(logTag, "CanvasView(Context) Ended");
 	}
 	
-	public CanvasView(Context context, AttributeSet attrs) {
+	public CanvasView(Context context, AttributeSet attrs) 
+	{
 		super(context, attrs);
-		// TODO Auto-generated constructor stub
+		Log.v(logTag, "CanvasView(Context, AtributeSet) Started");
 		SetupDrawing();
+		Log.v(logTag, "CanvasView(Context, AtributeSet) Ended");
 	}
 	
-	public CanvasView(Context context, AttributeSet attrs, int defStyleAttr) {
+	public CanvasView(Context context, AttributeSet attrs, int defStyleAttr) 
+	{
 		super(context, attrs, defStyleAttr);
-		// TODO Auto-generated constructor stub
+		Log.v(logTag, "CanvasView(Context, AtributeSet, int) Started");
 		SetupDrawing();
+		Log.v(logTag, "CanvasView(Context, AtributeSet, int) Ended");
 	}
 	
-	private void SetupDrawing(){
-		drawPath = new Path();
-		drawPaint = new Paint();
+	private void SetupDrawing()
+	{
+
+		Log.v(logTag, "SetupDrawing() started");
+		path = new Path();
+		paint = new Paint();
 		
-		drawPaint.setColor(paintColor);
-		drawPaint.setAntiAlias(true);
-		drawPaint.setStrokeWidth(10);
-		drawPaint.setStyle(Paint.Style.STROKE);
-		drawPaint.setStrokeJoin(Paint.Join.ROUND);
-		drawPaint.setStrokeCap(Paint.Cap.ROUND);
+		paint.setColor(paintColor);
+		paint.setAntiAlias(true);
+		paint.setStrokeWidth(1);
+		paint.setStyle(Paint.Style.STROKE);
+		paint.setStrokeJoin(Paint.Join.ROUND);
+		paint.setStrokeCap(Paint.Cap.ROUND);
 		
 		canvasPaint = new Paint(Paint.DITHER_FLAG);
-		
-		
+		rotatePositive = -1;
+		Log.v(logTag, "SetupDrawing() ended");
 	}
 
 	@Override
-	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-	//view given size
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) 
+	{
+		Log.v(logTag, "onSizeChanged(w="+w+", h="+h+", oldw="+oldw+", oldh="+oldh+") Started");
 		super.onSizeChanged(w, h, oldw, oldh);
-		canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+		//if(canvasBitmap == null)
+		//{
+			//Log.v(logTag, "canvasBitmap was null");
+		//}
+		if(oldw==h && oldh==w)
+		{
+			Log.v(logTag, "Screen was reoriented");
+			Matrix matrix = new Matrix();
+			matrix.postRotate(rotatePositive*90f);
+			Bitmap rotatedBitmap = Bitmap.createBitmap(canvasBitmap, 0, 0, oldw, oldh, matrix, false);
+			//canvasBitmap.recycle();
+			canvasBitmap = Bitmap.createBitmap(rotatedBitmap);
+			rotatePositive = rotatePositive * -1;
+			//rotatedBitmap.recycle();
+		}
+		else
+		{
+			Log.v(logTag, "Completely new screen found");
+			canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+		}
+		Log.v(logTag, "canvasBitmap set, Drawing to Canvas");
 		drawCanvas = new Canvas(canvasBitmap);
+		Log.v(logTag, "onSizeChanged(int,int,int,int) Ended");
 	}
 
 	@Override
-	protected void onDraw(Canvas canvas) {
+	protected void onDraw(Canvas canvas) 
+	{
 	//draw view
 		canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
-		canvas.drawPath(drawPath, drawPaint);
+		canvas.drawPath(path, paint);
 	}
 	
 	@Override
-	public boolean onTouchEvent(MotionEvent event) {
+	public boolean onTouchEvent(MotionEvent event) 
+	{
 	//detect user touch
 		float touchX = event.getX();
 		float touchY = event.getY();
 		
 		switch (event.getAction()) {
 		case MotionEvent.ACTION_DOWN:
-		    drawPath.moveTo(touchX, touchY);
+		    path.moveTo(touchX, touchY);
 		    break;
 		case MotionEvent.ACTION_MOVE:
-		    drawPath.lineTo(touchX, touchY);
+		    path.lineTo(touchX, touchY);
 		    break;
 		case MotionEvent.ACTION_UP:
-		    drawCanvas.drawPath(drawPath, drawPaint);
-		    drawPath.reset();
+		    drawCanvas.drawPath(path, paint);
+		    path.reset();
 		    break;
 		default:
 		    return false;
