86d2dcfdc9c463dd29fcf1054292d767f58ca62c
flide <flideravi@gmail.com>
2014-05-08 22:50:28 +0530

    Setting up some logging functions

--- src/inc/flide/touchboard/CanvasView.java
+++ src/inc/flide/touchboard/CanvasView.java
@@ -1,23 +1,45 @@
 package inc.flide.touchboard;
 
+import android.os.Build;
+import android.os.Bundle;
 import android.util.Log;
-import android.content.Context;
 import android.util.AttributeSet;
+import android.content.Context;
+
 import android.view.View;
 import android.view.Display;
+import android.view.MotionEvent;
+
 import android.graphics.Canvas;
 import android.graphics.Bitmap;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Matrix;
-import android.view.MotionEvent;
-import android.content.Context;
 import android.hardware.display.DisplayManager;
 
 public class CanvasView extends View 
 {
 
-	private static final String logTag = "CanvasView";
+	//Adding the Debug variables and functions
+	private static final String CLASS_NAME = "CanvasView";
+	private static final boolean DEBUG = true;
+	private static final boolean VERBOSE = true;
+
+	void Verbose(String message)
+	{
+		 if(VERBOSE == true)
+			 Log.v(CLASS_NAME, message);
+			  
+	}
+
+	void Debug(String message)
+	{
+		 if(DEBUG == true)
+			 Log.d(CLASS_NAME, message);
+			  
+	}
+	//Debuging segment ends
+	
 	static int currentDisplayOrientation;
 	//drawing path
 	private Path path;
@@ -33,31 +55,31 @@ public class CanvasView extends View
 	public CanvasView(Context context) 
 	{
 		super(context);
-		Log.v(logTag, "CanvasView(Context) Started");
+		Verbose("CanvasView(Context) Started");
 		SetupDrawing();
-		Log.v(logTag, "CanvasView(Context) Ended");
+		Verbose("CanvasView(Context) Ended");
 	}
 	
 	public CanvasView(Context context, AttributeSet attrs) 
 	{
 		super(context, attrs);
-		Log.v(logTag, "CanvasView(Context, AtributeSet) Started");
+		Verbose("CanvasView(Context, AtributeSet) Started");
 		SetupDrawing();
-		Log.v(logTag, "CanvasView(Context, AtributeSet) Ended");
+		Verbose("CanvasView(Context, AtributeSet) Ended");
 	}
 	
 	public CanvasView(Context context, AttributeSet attrs, int defStyleAttr) 
 	{
 		super(context, attrs, defStyleAttr);
-		Log.v(logTag, "CanvasView(Context, AtributeSet, int) Started");
+		Verbose("CanvasView(Context, AtributeSet, int) Started");
 		SetupDrawing();
-		Log.v(logTag, "CanvasView(Context, AtributeSet, int) Ended");
+		Verbose("CanvasView(Context, AtributeSet, int) Ended");
 	}
 	
 	private void SetupDrawing()
 	{
 
-		Log.v(logTag, "SetupDrawing() started");
+		Verbose("SetupDrawing() started");
 		path = new Path();
 		paint = new Paint();
 		
@@ -69,13 +91,13 @@ public class CanvasView extends View
 		paint.setStrokeCap(Paint.Cap.ROUND);
 		
 		canvasPaint = new Paint(Paint.DITHER_FLAG);
-		Log.v(logTag, "SetupDrawing() ended");
+		Verbose("SetupDrawing() ended");
 	}
 
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) 
 	{
-		Log.v(logTag, "onSizeChanged(w="+w+", h="+h+", oldw="+oldw+", oldh="+oldh+") Started");
+		Verbose("onSizeChanged(w="+w+", h="+h+", oldw="+oldw+", oldh="+oldh+") Started");
 		super.onSizeChanged(w, h, oldw, oldh);
 
 		DisplayManager displayManager = (DisplayManager)getContext().getSystemService(Context.DISPLAY_SERVICE);
@@ -83,13 +105,13 @@ public class CanvasView extends View
 
 		if(oldw==0 && oldh==0)
 		{
-			Log.v(logTag, "Application Initialized");
+			Verbose("Application Initialized");
 			canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
 			currentDisplayOrientation = display.getRotation();
 		}
 		else if( currentDisplayOrientation != display.getRotation())
 		{
-			Log.v(logTag, "Screen was reoriented");
+			Verbose("Screen was reoriented");
 			Matrix matrix = new Matrix();
 			int previousDisplayOrientation = currentDisplayOrientation;
 			currentDisplayOrientation = display.getRotation();
@@ -100,9 +122,9 @@ public class CanvasView extends View
 			canvasBitmap = Bitmap.createBitmap(rotatedBitmap);
 			rotatedBitmap.recycle();
 		}
-		Log.v(logTag, "canvasBitmap set, Drawing to Canvas");
+		Verbose("canvasBitmap set, Drawing to Canvas");
 		drawCanvas = new Canvas(canvasBitmap);
-		Log.v(logTag, "onSizeChanged(int,int,int,int) Ended");
+		Verbose("onSizeChanged(int,int,int,int) Ended");
 	}
 
 	@Override
