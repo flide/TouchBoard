1ba6dd668011f19a5736b4f05f49d0091385333a
flide <flideravi@gmail.com>
2014-05-21 08:47:18 +0530

    Modularized the TouchHandling
    Modularized the touch-handling in the Pen class. Next step would be to
    enforce this modularization in all the tools.

--- src/inc/flide/touchboard/tools/Pen.java
+++ src/inc/flide/touchboard/tools/Pen.java
@@ -12,10 +12,14 @@ import android.graphics.Path;
 
 public class Pen extends Brush
 {
+	private boolean penDownFlag,penMoveFlag,penUpFlag;
 	public Pen(CanvasModel model)
 	{
 		super(model);
 		setPaintAttributes();
+		clearPenDownFlag();
+		clearPenMoveFlag();
+		clearPenUpFlag();
 	}
 
 	public boolean handleTouchEvent(MotionEvent event)
@@ -25,30 +29,32 @@ public class Pen extends Brush
 		{
 		case MotionEvent.ACTION_DOWN:
 			onPenDown(event);
-		    break;
+			break;
 		case MotionEvent.ACTION_MOVE:
-		    onPenMove(event);
-		    break;
+			onPenMove(event);
+			break;
 		case MotionEvent.ACTION_UP:
-		    onPenUp(event);
-		    break;
+			onPenUp(event);
+			break;
 		default:
-		    return false;
+			return false;
 		}
 		return true;
 	}
 
 	public void onPenDown(MotionEvent event)
 	{
+		setPenDownFlag();
 		float x = event.getX();
 		float y = event.getY();
 		 
 		path.moveTo(x, y);
-		path.addCircle(x, y, paint.getStrokeWidth()/2.0f,Path.Direction.CW);
-		model.updateBitmap(path,paint);
+		//model.updateBitmap(path,paint);
 	}
 	public void onPenMove(MotionEvent event)
 	{
+		setPenMoveFlag();
+		clearPenDownFlag();
 		float x = event.getX();
 		float y = event.getY();
 		path.lineTo(x, y);
@@ -56,10 +62,24 @@ public class Pen extends Brush
 	}
 	public void onPenUp(MotionEvent event)
 	{
+		setPenUpFlag();
+
 		float x = event.getX();
 		float y = event.getY();
-		model.updateBitmap(path,paint);
-		path.rewind();
+		
+		if(penDownFlag == true)
+		{
+			path.addCircle(x, y, paint.getStrokeWidth()/2.0f,Path.Direction.CW);
+			model.updateBitmap(path,paint);
+			clearPenDownFlag();
+		}
+		else if(penMoveFlag == true)
+		{
+			model.updateBitmap(path,paint);
+			clearPenMoveFlag();
+		}
+		path.reset();
+		clearPenUpFlag();
 	}
 
 	public void setPaintAttributes()
@@ -80,4 +100,17 @@ public class Pen extends Brush
 		paint.setDither(true);
 		Logger.Verbose(this.getClass().getName(), "Ending setPaintAttributes()");
 	}
+
+	private void setPenDownFlag()
+	{ penDownFlag = true; }
+	private void clearPenDownFlag()
+	{ penDownFlag = false; }
+	private void setPenMoveFlag()
+	{ penMoveFlag = true; }
+	private void clearPenMoveFlag()
+	{ penMoveFlag = false; }
+	private void setPenUpFlag()
+	{ penUpFlag = true; }
+	private void clearPenUpFlag()
+	{ penUpFlag = false; }
 }
