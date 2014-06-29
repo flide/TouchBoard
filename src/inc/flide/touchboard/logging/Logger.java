0574c127c9f21f2f4a4ebee972c63135c9c9cc34
flide <flideravi@gmail.com>
2014-06-29 17:15:34 +0530

    Updated Logging class
    All the logging functions available in android.util.Log class have
    been wrapped over for easier logging.

--- src/inc/flide/touchboard/logging/Logger.java
+++ src/inc/flide/touchboard/logging/Logger.java
@@ -4,16 +4,77 @@ import android.util.Log;
 
 public class Logger implements LoggingConstants
 {
-	public static void Verbose(String className, String message)
+	public static string tag;
+	
+	public static setTag(string className)
 	{
-		 if(LoggingConstants.VERBOSE == true)
-			 Log.v(LoggingConstants.Project_Name, className + " : " + message);
-			  
+		this.tag = LoggingConstants.Project_Name+className;
 	}
 
-	public static void Debug(String className, String message)
+	public static void Debug(String message)
 	{
 		 if(LoggingConstants.DEBUG == true)
-			 Log.d(LoggingConstants.Project_Name, className + " : " + message);
+			 Log.d(tag, className + " : " + message);
 	}
+
+	public static void Debug(String message, Throwable tr)
+	{
+		 if(LoggingConstants.DEBUG == true)
+			 Log.d(tag, className + " : " + message, tr);
+	}
+
+	public static void Error(String message)
+	{
+		 if(LoggingConstants.ERROR == true)
+			 Log.e(tag, className + " : " + message);
+	}
+
+	public static void Error(String message, Throwable tr)
+	{
+		 if(LoggingConstants.ERROR == true)
+			 Log.e(tag, className + " : " + message, tr);
+	}
+
+	public static void Info(String message)
+	{
+		 if(LoggingConstants.INFO == true)
+			 Log.i(tag, className + " : " + message);
+	}
+
+	public static void Info(String message, Throwable tr)
+	{
+		 if(LoggingConstants.INFO == true)
+			 Log.i(tag, className + " : " + message, tr);
+	}
+	
+	public static void Verbose(String message)
+	{
+		 if(LoggingConstants.VERBOSE == true)
+			 Log.v(tag, className + " : " + message);
+	}
+
+	public static void Verbose(String message, Throwable tr)
+	{
+		 if(LoggingConstants.VERBOSE == true)
+			 Log.v(tag, className + " : " + message, tr);
+	}
+
+	public static void Warn(String message)
+	{
+		 if(LoggingConstants.WARN == true)
+			 Log.w(tag, className + " : " + message);
+	}
+
+	public static void Warn(Throwable tr)
+	{
+		 if(LoggingConstants.WARN == true)
+			 Log.w(tag, tr);
+	}
+
+	public static void Warn(String message, Throwable tr)
+	{
+		 if(LoggingConstants.WARN == true)
+			 Log.w(tag, className + " : " + message, tr);
+	}
+
 }
--- src/inc/flide/touchboard/logging/LoggingConstants.java
+++ src/inc/flide/touchboard/logging/LoggingConstants.java
@@ -3,6 +3,10 @@ package inc.flide.touchboard.logging;
 public interface LoggingConstants
 {
 	String Project_Name = "TouchBoard";
+	boolean ASSERT = true;
 	boolean DEBUG = true;
+	boolean ERROR = true;
+	boolean INFO = true;
 	boolean VERBOSE = true;
+	boolean WARN = true;
 }
